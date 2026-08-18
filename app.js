5bec936e6cf11c6cbcab7c221a5a84b5e9f20f53
bhaveshdewangan1234-sys <bhaveshdewangan1234@gmail.com>
2026-08-18 10:51:54 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -5809,7 +5809,9 @@ document.addEventListener('DOMContentLoaded', () => {
                 `;
             } else if (type === 'googledrive') {
                 container.innerHTML = `
-                    <iframe src="${embedUrl}" class="w-full h-full border-0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                    <div class="w-full h-full overflow-hidden relative">
+                        <iframe src="${embedUrl}" class="absolute w-full border-0" style="top: -60px; left: 0; height: calc(100% + 60px);" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                    </div>
                 `;
             }
 
